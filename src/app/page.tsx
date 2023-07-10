6715a8dbbbff5d632aca2b32b7f08ffd9f56ea95
Alex Silva <alexcssilva@gmail.com>
2023-07-10 18:12:03 -0300

    feat: :sparkles: Implementing the Technologies component on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/Header/Header';
 import { HomeContainer } from '../styles/HomeStyles';
 import { HomeSection } from '../components/HomeSection/HomeSection';
+import { Technologies } from '../components/Technologies/Technologies';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
 
       <main className="container">
         <HomeSection />
+        <Technologies />
       </main>
     </HomeContainer>
   );
