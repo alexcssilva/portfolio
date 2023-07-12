1b7dec848d2b318b4b3af578da05ddd686d71f5a
Alex Silva <alexcssilva@gmail.com>
2023-07-11 21:37:22 -0300

    feat: :sparkles: Implementing the Projects component on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from '../components/Header/Header';
 import { HomeContainer } from '../styles/HomeStyles';
 import { HomeSection } from '../components/HomeSection/HomeSection';
 import { Technologies } from '../components/Technologies/Technologies';
+import { Projects } from '../components/Projects/Projects';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <main className="container">
         <HomeSection />
         <Technologies />
+        <Projects />
       </main>
     </HomeContainer>
   );
