d213a8298d5cfb4c17da255689853b07bab6d2f6
Alex Silva <alexcssilva@gmail.com>
2023-07-17 20:58:02 -0300

    feat: :sparkles: BannerProject starter component created

---
+++ src/components/BannerProject/BannerProject.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Container } from './BannerProject.styles';
+
+interface BannerProjectProps {
+  title: string;
+  type: string;
+  imgUrl: string;
+}
+
+export function BannerProject({ title, type, imgUrl }: BannerProjectProps) {
+  return (
+    <Container imgUrl={imgUrl}>
+      <div className="overlay" />
+      <section>
+        <h1>{title}</h1>
+        <h2>{type}</h2>
+      </section>
+    </Container>
+  );
+}
