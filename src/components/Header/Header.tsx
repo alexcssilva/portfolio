7d48cfb03e345e61c8c5fe577134e4698df7c410
Alex Silva <alexcssilva@gmail.com>
2023-07-20 21:42:10 -0300

    feat: :triangular_flag_on_post: Implementation of the scroll logic in the Header and the click in the page section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,31 @@
-'use client'
+'use client';
 
 import NavLink from './NavLink';
 import { Container } from './Header.styles';
-import { MdDarkMode } from 'react-icons/md';
+import { useEffect, useState } from 'react';
 
 export function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsScrolled(window.scrollY > 0);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Container>
+    <Container isTransparent={isScrolled}>
       <ul>
-        <NavLink title="Início" path="/" />
-        <NavLink title="Sobre" path="/" />
-        <NavLink title="Habilidades" path="/" includes/>
-        <NavLink title="Projetos" path="/" />
-        <NavLink title="Contato" path="/" />
-        <MdDarkMode size={24} color='black'/>
+        <NavLink title="Início" path="/#home" />
+        <NavLink title="Habilidades" path="/#skills" includes />
+        <NavLink title="Projetos" path="/#projects" />
+        <NavLink title="Contato" path="/#contact" />
       </ul>
     </Container>
   );
